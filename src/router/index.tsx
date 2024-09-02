24862d3e80c7c71d6cf924ddef2f0e7d4d428bd4
wdk <fredddray@gmail.com>
2024-09-03 00:03:43 +0800

    init

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,5 @@
 import App from "../App";
+import Editor from "../components/Editor/Editor";
 import Workspace from "../components/Worskpace/Workspace";
 import { createBrowserRouter } from "react-router-dom";
 
@@ -16,5 +17,9 @@ export const router = createBrowserRouter([
   {
     path: "*",
     element: <App />,
+  },
+  {
+    path: "/editor",
+    element: <Editor />,
   },
 ]);
